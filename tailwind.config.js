6f375f2eb31ed24c49520a9a0e2e876a12f59ea0
Túlio Ribeiro <tuliocribeiro@gmail.com>
2025-04-15 18:29:29 -0300

    fix: colors for md code

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,20 @@ module.exports = {
             "--tw-prose-headings": "var(--tapp-titles)", // Headings color
             "--tw-prose-links": "var(--tapp-links)", // Links color
             "--tw-prose-links-hover": "var(--tapp-titles)", // Links hover color
-            "--tw-prose-code": "var(--tapp-links)", // Code color
+            "--tw-prose-code": "var(--tapp-titles)", // Code color
             "--tw-prose-bullets": "var(--tapp-titles)", // Bullet points color
             "--tw-prose-quotes": "var(--tapp-text)", // Quote color
             "--tw-prose-quote-borders": "var(--tapp-box)", // Quote border color
+            "--tw-prose-quote-borders": "var(--tapp-box)", // Quote border color
+            code: {
+              "&::before": { content: "none" }, // Remove the ` before
+              "&::after": { content: "none" }, // Remove the ` after,
+              border: "1px solid rgba(255,255,255,.08)",
+              borderRadius: "0.25rem", // Rounded corners
+              fontWeight: "500", // Medium weight
+              fontFamily: "monospace, monospace", // Monospace font
+              padding: "0.2rem 0.4rem", // Padding for readability
+            },
           },
         },
       },
