5a2c0d67b9ac429a669696dcdc78c2c5c8326ab9
Túlio Ribeiro <tuliocribeiro@gmail.com>
2025-04-15 14:27:50 -0300

    chore: add vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://tulio.app",
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
